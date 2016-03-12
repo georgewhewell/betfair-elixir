72b357a48aed4e74f37d59b64654735c22432ede
George Whewell <georgerw@gmail.com>
2016-03-12 16:00:24 +0000

    careful now..

--- config/dev.exs
+++
@@ -1,9 +0,0 @@
-use Mix.Config
-
-config :betfair, [
-  "username": "barack",
-  "password": "obama",
-  "certfile": "test/fixture/certs/test.crt",
-  "keyfile": "test/fixture/certs/test.key",
-  "app_key": "app_k3y",
-  "session_token": "535510nt0k3n"]
---
+++ config/test.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+config :betfair, [
+  "username": "barack",
+  "password": "obama",
+  "certfile": "test/fixture/certs/test.crt",
+  "keyfile": "test/fixture/certs/test.key",
+  "app_key": "app_k3y",
+  "session_token": "535510nt0k3n"]
+
+config :exvcr, [
+  filter_sensitive_data: [
+    # [pattern: "sessionToken\"\:\".+\"", placeholder: "sessionToken\"\:\"sessionToken\""]
+  ],
+  filter_url_params: false,
+  response_headers_blacklist: ["Set-Cookie", "Server", "X-Authentication", "X-Application"],
+  vcr_cassette_library_dir: "test/fixture/vcr_cassettes"
+]
