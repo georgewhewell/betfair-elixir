8ca394d56dc1bd399d17a69b819ea2b95f14a3be
George Whewell <georgerw@gmail.com>
2016-03-12 16:25:37 +0000

    careful now..

--- config/test.exs
+++ config/test.exs
@@ -5,8 +5,7 @@ config :betfair, [
   "password": "obama",
   "certfile": "test/fixture/certs/test.crt",
   "keyfile": "test/fixture/certs/test.key",
-  "app_key": "app_k3y",
-  "session_token": "535510nt0k3n"]
+  "app_key": "app_k3y"]
 
 config :exvcr, [
   filter_sensitive_data: [
